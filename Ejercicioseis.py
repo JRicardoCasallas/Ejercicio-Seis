bc0eb47a007aeb67cf022702c56ae06478dc6537
Jose Ricado Casallas <josericardocasasllas11@gmail.com>
2025-01-09 18:28:28 -0500

    Ejercicio # 6

---
+++ Ejercicioseis.py
@@ -0,0 +1,4 @@
+"""Ejercicio 6. 
+Se   tiene   los   puntos   A   y   B   en   el   cuadrante   positivo   
+del   plano   cartesiano, elabore el algoritmo que permita obtener 
+la distancia entre A y B. """
